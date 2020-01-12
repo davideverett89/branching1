2d5b82dbcf9e0abaea108dfc572e36aec7539090
David Everett <davideverett1989@gmail.com>
2020-01-11 20:20:31 -0600

    Changed words, added some formatting, added basic EventListener.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("This is working!");
+let myButton = document.getElementById("button");
+let targetDiv = document.getElementById("demo");
+let word = window.prompt("Pick any random word.");
+myButton.addEventListener("click", makeWords);
+
+function makeWords() {
+    targetDiv.innerHTML = `<h1 class="new-class">${word}</h1>`;
+}
+
