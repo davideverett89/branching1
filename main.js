8712f4f5e4d24557c51bb1dd378fb37c594dc3b8
David Everett <davideverett1989@gmail.com>
2020-01-12 17:52:42 -0600

    Edited personal info & favorite dino description. Changed some font and colors.

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ let targetDiv = document.getElementById("demo");
 myButton.addEventListener("click", makeWords);
 
 function makeWords() {
-    targetDiv.innerHTML = `<h1 class="new-class">David Everett</h1>
+    targetDiv.innerHTML = `<h1 class="new-class">About Me</h1>
     <br>
     <img src="https://avatars2.githubusercontent.com/u/59631645?s=460&v=4 alt="Picture of me"/>
     <br>
-    <p>Great to meet you!  My name is David and I recently turned 30 years old.  I studied music, but I hope to also one day become a web developer!</p>`;
+    <p>My name is David Everett and I'm from central Illinois.  I have been living in Nashville for abouts 5 years now.  I studied music in college but now have a blossoming interest in web development and I'm excited to see where this takes me.</p>`;
 }
 
 let newButton = document.getElementById("button-2");
@@ -20,6 +20,6 @@ function moreWords() {
     <h3>Tyrannosaurus Rex</h3>
     <br>
     <img src="https://scitechdaily.com/images/Tyrannosaurus-rex-Dinosaur.jpg" alt="T-Rex">
-    <p>As a child the T-Rex was always my favorite because at the time it was the meanest dinosaur around!  Bigger, meaner dinosaurs have been discovered since then, but I gotta stay true to my guy T-Rex.</p>`;
+    <p>As a child, the T-Rex was always my favorite because at the time it was known as the most dangerous predator on Earth</p>`;
 }
 
